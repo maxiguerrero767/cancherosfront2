e877ecf9d77583a4b964ed3b3475e67136ffab3f
Tali <talicabana@gmail.com>
2025-12-08 18:54:23 -0300

    Modifiqué los márgenes y padding

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -5,12 +5,12 @@ import { Container, Row, Col } from "react-bootstrap";
 const Footer = () => {
   return (
     <Container className="">
-      <Row className="links-section bg-black text-white py-4">
+      <Row className="links-section bg-black text-white pt-4">
         <div className="container">
-          <div className="row text-center text-md-start">
+          <div className="row text-center text-md-start pb-2">
             {/* Columna Contactanos */}
-            <div className="col-md-4 mb-4 mb-md-0 text-center">
-              <h6 className="text-warning fw-bold mb-3">Contactanos</h6>
+            <div className="col-md-4 mb-3 text-center">
+              <h6 className="titulos-redes fw-bold mb-1">Contactanos</h6>
               <div className="d-flex justify-content-center gap-3 social-icons">
                 <i className="bi bi-whatsapp"></i>
                 <i className="bi bi-envelope"></i>
@@ -18,8 +18,8 @@ const Footer = () => {
             </div>
 
             {/* Columna Redes */}
-            <div className="col-md-4 mb-4 mb-md-0 text-center">
-              <h6 className="text-warning fw-bold mb-3">Nuestras redes</h6>
+            <div className="col-md-4 mb-3 text-center">
+              <h6 className="titulos-redes fw-bold mb-1">Nuestras redes</h6>
               <div className="d-flex justify-content-center gap-3 social-icons">
                 <i className="bi bi-instagram"></i>
                 <i className="bi bi-facebook"></i>
@@ -28,8 +28,8 @@ const Footer = () => {
             </div>
 
             {/* Columna Descarga */}
-            <div className="col-md-4 mb-4 mb-md-0 text-center">
-              <h6 className="text-warning fw-bold mb-3">Descargá la app</h6>
+            <div className="col-md-4 mb-1 text-center">
+              <h6 className="titulos-redes fw-bold mb-1">Descargá la app</h6>
               <div className="d-flex justify-content-center gap-3 social-icons">
                 <i class="bi bi-apple"></i>
                 <i class="bi bi-google-play"></i>
@@ -37,7 +37,7 @@ const Footer = () => {
             </div>
           </div>
 
-          <div className="text-center mt-4 pt-3 border-top border-secondary">
+          <div className="text-center mb-4 pt-3 border-top border-secondary">
             <small className="text-muted">
               © Copyright 2024. Todos los derechos reservados.
             </small>
