5264cc1e3dc7890208351f3340bef0c953053074
Tali <talicabana@gmail.com>
2025-12-11 02:53:04 -0300

    Quite estilo del componente que va en el css (del navbar)

--- src/components/shared/Menu.jsx
+++ src/components/shared/Menu.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../../styles/menu.css";
 import { Link } from "react-router-dom";
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 import logoDibuNeon17 from "../../img/logoDibuNeon17.png";
 
 const Menu = ({
@@ -13,21 +13,16 @@ const Menu = ({
 }) => {
   return (
     <header className="navbar-oscuro">
-      <Link to="/" className="text-decoration-none text-white">
+      <Link to="/" className="text-decoration-none">
         <div className="logo-container">
-{/*           <span className="logo-icon">⚽</span>
- */}         
-           <img 
-            src={logoDibuNeon17}
-            alt="logo cancheros" 
-            className="logo-img"
-          />
-  {/* <span className="logo-text">Cancheros TRUCHO</span> */}
+          {/*           <span className="logo-icon">⚽</span>
+           */}
+          <img src={logoDibuNeon17} alt="logo cancheros" className="logo-img" />
+          {/* <span className="logo-text">Cancheros TRUCHO</span> */}
         </div>
       </Link>
 
       <nav className="nav-links">
-        
         <Link to="/" className="nav-item">
           Inicio
         </Link>
@@ -43,7 +38,9 @@ const Menu = ({
           </>
         ) : usuario ? (
           <>
-            <span className="nav-link-light me-2"><i className="bi bi-person-plus"></i>  {usuario}</span>
+            <span className="nav-link-light me-2">
+              <i className="bi bi-person-plus"></i> {usuario}
+            </span>
             <span className="nav-link-light" onClick={cerrarSesion}>
               Cerrar sesión
             </span>
