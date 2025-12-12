d4bcafe763414d320a2bfb569cda663f80cb7a37
Maximiliano <maximilianoguerreromeriles@gmail.com>
2025-12-12 19:11:31 -0400

    udpate libreria react

--- src/components/pages/administrador.jsx
+++ src/components/pages/administrador.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect  } from "react";
 import { Container, Tabs, Tab } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Swal from "sweetalert2";
